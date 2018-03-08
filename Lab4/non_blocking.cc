03e8a2601380f4b5253da1743acbc9940434406e
Anton Pupkov <pupant@gmail.com>
2018-03-08 09:45:11 -0800

    Work on nonblocking

--- Lab4/non_blocking.cc
+++ Lab4/non_blocking.cc
@@ -22,27 +22,8 @@ bool
 NonBlockingCache::receiveRequest(uint64_t address, int size,
                                 const uint8_t* data, int request_id)
 {
-/*
-Algorithm idea:
-
-if(isMiss)
-{
-  If(NOT in lookup_table)
-  {
-    if(lookup_table has reached max size)
-    {
-      return false
-    }
-  }
-}
-
-SetAssociativeCache::receiveRequest();
-blocked = false; // Manually reset blocking bit
-
-return true
-*/
     bool isFound = false;
-    bool isMiss = !hit(address);
+    bool isMiss = !SetAssociativeCache::hit(address);
 
     if(isMiss)
     {
@@ -56,23 +37,20 @@ return true
         }
       }
 
-      if(!isFound)
+      if(!isFound && lookup_table.size() >= max_mshr_requests)
       {
-        if(lookup_table.size() >= max_mshr_requests)
-        {
-          return false;
-        }
+        return false;
       }
     }
 
     SetAssociativeCache::receiveRequest(address, size, data, request_id);
-    blocked = false;
 
     if(!isFound && isMiss)
     {
-      lookup_table.push_back(getMSHR());
+      lookup_table.push_back(SetAssociativeCache::getMSHR());
 
-      clearMSHR();
+      SetAssociativeCache::clearMSHR();
+      blocked = false;
     }
 
     return true;
@@ -81,23 +59,12 @@ return true
 void
 NonBlockingCache::receiveMemResponse(int request_id, const uint8_t* data)
 {
-/*
-Algorithm idea:
-
-if(request_id is in lookup_table)
-{
-
-  SetAssociativeCache::receiveMemResponse();
-
-}
-*/
-
-  for (auto lt_it = lookup_table.begin(); lt_it != lookup_table.end();)
+  for (auto lt_it = lookup_table.begin(); lt_it != lookup_table.end(); ++lt_it)
   {
     if(request_id == lt_it->savedId)
     {
       //We found it in the lookup table
-      setMSHR(*lt_it);
+      SetAssociativeCache::setMSHR(*lt_it);
       SetAssociativeCache::receiveMemResponse(request_id, data);
 
       lookup_table.erase(lt_it);
