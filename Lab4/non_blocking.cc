51d3cea69826df01b329314f57b994a818248d3b
richardli <srli@ucdavis.edu>
2018-03-11 16:05:30 -0700

    Bug fix
    Fixed an issue that causes incorrect data to be written to the memory.
    More testing required.

--- Lab4/non_blocking.cc
+++ Lab4/non_blocking.cc
@@ -94,11 +94,11 @@ NonBlockingCache::receiveRequest(uint64_t address, int size,
         sendMemRequest(wb_address, memory.getLineSize(), line, request_id);
       }
     }
+    lookup_table.push_back(mshr);
+    
     tagArrayVec[insertIndex]->setState(set, Invalid);
     uint64_t block_address = address & ~(memory.getLineSize() -1);
-    lookup_table.push_back(mshr);
     sendMemRequest(block_address, memory.getLineSize(), nullptr, request_id);
-    
   }
   return true;
 }
@@ -106,34 +106,45 @@ NonBlockingCache::receiveRequest(uint64_t address, int size,
 void
 NonBlockingCache::receiveMemResponse(int request_id, const uint8_t* data)
 {
-//  assert(request_id == 0); not needed because valid request_id is required
+  //  assert(request_id == 0); not needed because valid request_id is required
   assert(data);
-
+  
   std::vector<MSHR>::iterator lt_it;
   for (lt_it = lookup_table.begin(); lt_it!= lookup_table.end(); lt_it++)
   {
-    if (lt_it->savedId == request_id)
-    {
-      break;
-    }
+    if (lt_it->savedId == request_id) break;
   }
   
   int set = (int) getSet(lt_it->savedAddr);
-  int insertIndex = lt_it->savedInsertIndex;
-  uint8_t* line = dataArrayVec[insertIndex]->getLine(set);
-  std::memcpy(line, data, memory.getLineSize());
+  int insertIndex;
+  uint8_t* line;
   
-  assert(tagArrayVec[insertIndex]->getState(set) == Invalid);
-  
-  tagArrayVec[insertIndex]->setState(set, Valid);
-  
-  tagArrayVec[insertIndex]->setTag(set, getTag(lt_it->savedAddr));
+  if (!hit(lt_it->savedAddr)) // miss
+  {
+    insertIndex = lt_it->savedInsertIndex;
+    line = dataArrayVec[insertIndex]->getLine(set);
+    // previous entry in MSHR not resolve current request
+    std::memcpy(line, data, memory.getLineSize());
+    
+//  assert(tagArrayVec[insertIndex]->getState(set) == Invalid);
+//  not needed because MSHR could previously set this to Dirty and cause trouble
+    
+    tagArrayVec[insertIndex]->setState(set, Valid);
+    
+    tagArrayVec[insertIndex]->setTag(set, getTag(lt_it->savedAddr));
+  }
+  else // hit
+  {
+    insertIndex = hitArray(lt_it->savedAddr);
+    line = dataArrayVec[insertIndex]->getLine(set);
+    // if previous MSHR resolve current one, free 1 tag array
+    tagMap[set]--;
+  }
   
   int block_offset = (int) getBlockOffset(lt_it->savedAddr);
-  
   if (lt_it->savedData) // write
   {
-    memcpy(&line[block_offset], lt_it->savedData, lt_it->savedSize);
+    std::memcpy(&line[block_offset], lt_it->savedData, lt_it->savedSize);
     sendResponse(lt_it->savedId, nullptr);
     tagArrayVec[insertIndex]->setState(set, Dirty);
   }
